d7e5503f0606f61483a1f313267d95356e1e00ba
alvessousaspa <80459995+alvessousaspa@users.noreply.github.com>
2024-07-19 10:44:22 -0300

    dsadas

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ const routes = require('./routes');
 
 const app = express();
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
-app.use(helmet());
 app.use('/', routes);
 
 // default catch all handler
