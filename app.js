ac40e9345fdb6d391983375f4e473ac6bae9e69a
alvessousaspa <80459995+alvessousaspa@users.noreply.github.com>
2024-07-19 11:27:19 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,20 +5,9 @@ const routes = require('./routes');
 var csrf = express.csrf();
 const app = express();
 
-var conditionalCSRF = function (req, res, next) {
-    //compute needCSRF here as appropriate based on req.path or whatever
-    if (needCSRF) {
-      csrf(req, res, next);
-    } else {
-      next();
-    }
-  }
-  
-app.use(conditionalCSRF);
 
-app.use('/', routes);
 
+app.use('/', routes, express.csrf());
 // default catch all handler
-app.all('*', (req, res) => res.status(404).json(new ErrorResponseObject('route not defined')));
 
 module.exports = app;
