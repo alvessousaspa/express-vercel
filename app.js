13b1a806ace6396acf581718d996e5424764b308
alvessousaspa <80459995+alvessousaspa@users.noreply.github.com>
2024-07-19 11:38:53 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,19 @@ const { ErrorResponseObject } = require('./common/http');
 const routes = require('./routes');
 const app = express();
 
+// Middleware para adicionar o cabeçalho Service-Worker-Allowed
+app.use((req, res, next) => {
+  res.setHeader('Service-Worker-Allowed', '/');
+  next();
+});
 
+// Usar Helmet para segurança
+app.use(helmet());
 
+// Suas rotas
 app.use('/', routes);
-// default catch all handler
+
+// Default catch-all handler
 app.all('*', (req, res) => res.status(404).json(new ErrorResponseObject('route not defined')));
 
 module.exports = app;
