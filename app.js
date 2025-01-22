f56b0f6dd6fdc9c31d91d7f6d1f6f3d76c32a724
alvessousaspa <80459995+alvessousaspa@users.noreply.github.com>
2025-01-22 08:49:38 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const helmet = require('helmet');
+const cors = require('cors'); // Importe a biblioteca cors
 const { ErrorResponseObject } = require('./common/http');
 const routes = require('./routes');
 const app = express();
@@ -12,6 +13,24 @@ app.use((req, res, next) => {
 
 // Usar Helmet para segurança
 app.use(helmet());
+
+// Configurar o CORS para permitir solicitações do seu domínio
+const allowedOrigins = ['https://m.bnbtiger.baby']; // Adicione outros domínios se necessário
+app.use(cors({
+  origin: function (origin, callback) {
+    // Permite solicitações sem origem (como as do Postman)
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'A política de CORS para este site não permite acesso a partir da origem especificada.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+
+// Adicione o middleware para analisar corpos de solicitação codificados em URL
+app.use(express.urlencoded({ extended: true }));
 
 // Suas rotas
 app.use('/', routes);
